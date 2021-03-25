4c83df832e8f834e8a67cfefc71ecb92ae916b66
Thibaut SANJUAN <sanjuan.thibaut@gmail.com>
2021-03-25 14:51:30 +0100

    feat: search
    Mise à jour de la recherche pour linux

--- helpers/find_files.py
+++ helpers/find_files.py
@@ -1,5 +1,9 @@
+import glob
 import os
 import re
+import time
+from datetime import timedelta
+
 import pandas as pd
 import numpy as np
 
@@ -16,14 +20,17 @@ def find_files_in_folder_yield(path, extension, contains_txt='', sub_folders=Tru
     if type(contains_txt) == str:  # if a string and not in a list
         contains_txt = [contains_txt]
 
-    my_regex_obj = re.compile('\.' + extension + '$')  # Makes sure the file extension is at the end and is preceded by a .
+    my_regex_obj = re.compile(
+        '\.' + extension + '$')  # Makes sure the file extension is at the end and is preceded by a .
 
     try:  # Trapping a OSError or FileNotFoundError:  File permissions problem I believe
         for entry in os.scandir(path):
             if entry.is_file() and my_regex_obj.search(entry.path):  #
-                bools = [True for txt in contains_txt if txt in entry.path and (exclude_text == '' or exclude_text not in entry.path)]
+                bools = [True for txt in contains_txt if
+                         txt in entry.path and (exclude_text == '' or exclude_text not in entry.path)]
                 if len(bools) == len(contains_txt):
-                    yield entry.stat().st_size, entry.stat().st_atime_ns, entry.stat().st_mtime_ns, entry.stat().st_ctime_ns, os.path.normpath(entry.path)
+                    yield entry.stat().st_size, entry.stat().st_atime_ns, entry.stat().st_mtime_ns, entry.stat().st_ctime_ns, os.path.normpath(
+                        entry.path)
 
             elif entry.is_dir() and sub_folders:  # if its a directory, then repeat process as a nested function
                 yield from find_files_in_folder_yield(entry.path, extension, contains_txt, sub_folders)
@@ -60,7 +67,36 @@ def find_path_of_files_in_folder_yield(path, extension, contains_txt='', sub_fol
 
     return paths
 
+
 # ext = 'txt'  # regular expression
 # containsTxt = []
 # path = 'C:\myFolder'
 # df = findFilesInFolderYieldandGetDf(path, ext, containsTxt, subFolders=True)
+
+
+def stopwatch(method):
+    def timed(*args, **kw):
+        ts = time.perf_counter()
+        result = method(*args, **kw)
+        te = time.perf_counter()
+        duration = timedelta(seconds=te - ts)
+        print(f"{method.__name__}: {duration}")
+        return result
+
+    return timed
+
+
+@stopwatch
+def get_filepaths_with_oswalk(root_path: str, file_regex: str):
+    files_paths = []
+    pattern = re.compile(file_regex)
+    for root, directories, files in os.walk(root_path):
+        for file in files:
+            if pattern.match(file):
+                files_paths.append(os.path.join(root, file))
+    return files_paths
+
+
+@stopwatch
+def get_filepaths_with_glob(root_path: str, file_regex: str):
+    return glob.glob(os.path.join(root_path, file_regex))
